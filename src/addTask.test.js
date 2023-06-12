d10d3444451f63e636dbff26451ef6cfd465bc04
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-12 20:31:55 +0630

    Implement test function for add and remove

---
+++ src/addTask.test.js
@@ -0,0 +1,15 @@
+import addTask from './modules/addTask.js';
+
+describe('addTask', () => {
+  const tasks = [
+    { description: 'Task1', completed: false },
+    { description: 'Task2', completed: false },
+  ];
+  test('should add a new task to the tasks array', () => {
+    const newTask = { description: 'Task3', completed: false };
+    const updatedTasks = addTask(tasks, newTask);
+
+    expect(updatedTasks).toHaveLength(3);
+    expect(updatedTasks[2]).toEqual(newTask);
+  });
+});
