e8b5bea2bbaa8c51a96511a49df0ae90ffebe91d
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-20 21:24:20 +0630

    remove checkbox-container id

--- src/modules/loadTask.js
+++ src/modules/loadTask.js
@@ -1,7 +1,7 @@
 const loadTask = (tasks) => {
   let taskListHTML = '';
   tasks.forEach((task, index) => {
-    taskListHTML += `<li class="d-flex align-items-center task p-4 justify-content-between"><div class="checkbox-container"><input type="checkbox" name="${task.description}" id="checkbox-container"><input type="text" value="${task.description}" class="description-text w-75"></div><i class="fa-solid fa-ellipsis-vertical fs-3" id=${index}></i></li>
+    taskListHTML += `<li class="d-flex align-items-center task p-4 justify-content-between"><div class="checkbox-container"><input type="checkbox" name="${task.description}"><input type="text" value="${task.description}" class="description-text w-75"></div><i class="fa-solid fa-ellipsis-vertical fs-3" id=${index}></i></li>
     `;
   });
   return taskListHTML;
